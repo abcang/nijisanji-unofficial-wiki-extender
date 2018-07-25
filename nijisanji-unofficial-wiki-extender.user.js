85923ce40ca837192ace377dd13e618866f24fb1
abcang <abcang1015@gmail.com>
2018-07-26 00:08:17 +0900

    通知機能をざっくりとつけた

--- nijisanji-unofficial-wiki-extender.user.js
+++ nijisanji-unofficial-wiki-extender.user.js
@@ -12,11 +12,27 @@
     'use strict';
 
     const { highlightSetting, HighlightSetting } = createHighlightSetting();
+    const timers = [];
+    const notifyBeforeMinutes = 10;
     let hideOlder = true;
     let hideCalendar = true;
     let interval = null;
+    let canNotify = false;
 
     initialize();
+
+    if (window.Notification) {
+        if (Notification.permission === 'granted') {
+            canNotify = true;
+        } else if (Notification.permission !== 'denied') {
+            // TODO: ボタンクリック時に確認ダイアログを出す
+            Notification.requestPermission((permission) => {
+                if (permission === 'granted') {
+                    canNotify = true;
+                }
+            });
+        }
+    }
 
     function initialize() {
         highlightAll();
@@ -31,6 +47,7 @@
             addOlderToggleButton();
 
             highlightAndDarkenRecentSchedule();
+            initializeNotificationTimer();
 
             if (!interval) {
                 interval = setInterval(() => {
@@ -54,57 +71,61 @@
             constructor() {
                 this.livers = [
                     // 1期生
-                    { key: 'tsukino_mito', name: '月ノ美兎' },
-                    { key: 'yuuki_chihiro', name: '勇気ちひろ' },
-                    { key: 'elu', name: 'える' },
-                    { key: 'higuchi_kaede', name: '樋口楓' },
-                    { key: 'shizuka_rin', name: '静凛' },
-                    { key: 'shibuya_hajime', name: '渋谷ハジメ' },
-                    { key: 'suzuya_aki', name: '鈴谷アキ' },
-                    { key: 'moira', name: 'モイラ' },
+                    { key: 'tsukino_mito', name: '月ノ美兎', icon: '/nijisanji/?plugin=ref&page=月ノ美兎&src=tsukino_mito.jpg' },
+                    { key: 'yuuki_chihiro', name: '勇気ちひろ', icon: '/nijisanji/?plugin=ref&page=勇気ちひろ&src=yuuki_chihiro.jpg' },
+                    { key: 'elu', name: 'える', icon: '/nijisanji/?plugin=ref&page=える&src=elu.jpg' },
+                    { key: 'higuchi_kaede', name: '樋口楓', icon: '/nijisanji/?plugin=ref&page=樋口楓&src=higuchi_kaede.jpg' },
+                    { key: 'shizuka_rin', name: '静凛', icon: '/nijisanji/?plugin=ref&page=静凛&src=shizuka_rin.jpg' },
+                    { key: 'shibuya_hajime', name: '渋谷ハジメ', icon: '/nijisanji/?plugin=ref&page=渋谷ハジメ&src=shibuya_hajime.jpg' },
+                    { key: 'suzuya_aki', name: '鈴谷アキ', icon: '/nijisanji/?plugin=ref&page=鈴谷アキ&src=suzuya_aki.jpg' },
+                    { key: 'moira', name: 'モイラ', icon: '/nijisanji/?plugin=ref&page=モイラ&src=moira.jpg' },
                     // 2期生
-                    { key: 'suzuka_utako', name: '鈴鹿詩子' },
-                    { key: 'ushimi_ichigo', name: '宇志海いちご' },
-                    { key: 'ienaga_mugi', name: '家長むぎ' },
-                    { key: 'yuuhi_riri', name: '夕陽リリ' },
-                    { key: 'mononobe_alice', name: '物述有栖' },
-                    { key: 'noraneko', name: '文野環' },
-                    { key: 'hushimi_gaku', name: '伏見ガク' },
-                    { key: 'giruzaren3rd', name: 'ギルザレンIII世' },
-                    { key: 'kenmochi_touya', name: '剣持刀也' },
-                    { key: 'morinaka_kazaki', name: '森中花咲' },
+                    { key: 'suzuka_utako', name: '鈴鹿詩子', icon: '/nijisanji/?plugin=ref&page=鈴鹿詩子&src=suzuka_utako.jpg' },
+                    { key: 'ushimi_ichigo', name: '宇志海いちご', icon: '/nijisanji/?plugin=ref&page=宇志海いちご&src=ushiumi_ichigo.jpg' },
+                    { key: 'ienaga_mugi', name: '家長むぎ', icon: '/nijisanji/?plugin=ref&page=家長むぎ&src=ienaga_mugi.jpg' },
+                    { key: 'yuuhi_riri', name: '夕陽リリ', icon: '/nijisanji/?plugin=ref&page=夕陽リリ&src=yuuhi_riri.jpg' },
+                    { key: 'mononobe_alice', name: '物述有栖', icon: '/nijisanji/?plugin=ref&page=物述有栖&src=mononobe_alice.jpg' },
+                    { key: 'noraneko', name: '文野環', icon: '/nijisanji/?plugin=ref&page=文野環&src=noraneko.jpg' },
+                    { key: 'hushimi_gaku', name: '伏見ガク', icon: '/nijisanji/?plugin=ref&page=伏見ガク&src=hushimi_gaku.jpg' },
+                    { key: 'giruzaren3rd', name: 'ギルザレンIII世', icon: '/nijisanji/?plugin=ref&page=ギルザレンIII世&src=giruzaren3rd.jpg' },
+                    { key: 'kenmochi_touya', name: '剣持刀也', icon: '/nijisanji/?plugin=ref&page=剣持刀也&src=kenmochi_touya.jpg' },
+                    { key: 'morinaka_kazaki', name: '森中花咲', icon: '/nijisanji/?plugin=ref&page=森中花咲&src=morinaka_kazaki_0.jpg' },
                     // ゲーマーズ
-                    { key: 'kanae', name: '叶' },
-                    { key: 'akabane_youko', name: '赤羽葉子' },
-                    { key: 'sasaki_saku', name: '笹木咲' },
-                    { key: 'yamiyono_moruru', name: '闇夜乃モルル' },
-                    { key: 'honma_himawari', name: '本間ひまわり' },
+                    { key: 'kanae', name: '叶', icon: '/nijisanji/?plugin=ref&page=叶&src=kanae_0.jpg' },
+                    { key: 'akabane_youko', name: '赤羽葉子', icon: '/nijisanji/?plugin=ref&page=赤羽葉子&src=ihTN2W2F_400x400.jpg' },
+                    { key: 'sasaki_saku', name: '笹木咲', icon: '/nijisanji/?plugin=ref&page=笹木咲&src=foX-dRp6_400x400.jpg' },
+                    { key: 'yamiyono_moruru', name: '闇夜乃モルル', icon: '/nijisanji/?plugin=ref&page=闇夜乃モルル&src=rnrrdark_400x400.jpg' },
+                    { key: 'honma_himawari', name: '本間ひまわり', icon: '/nijisanji/?plugin=ref&page=本間ひまわり&src=honmahimawari_400x400.jpg' },
                     // SEEDs
-                    { key: 'dola', name: 'ドーラ' },
-                    { key: 'umiyasyanokami', name: '海夜叉神' },
-                    { key: 'nakao_azuma', name: '名伽尾アズマ' },
-                    { key: 'izumo_kasumi', name: '出雲霞' },
-                    { key: 'todoroki_kyouko', name: '轟京子' },
-                    { key: 'sister_cleaire', name: 'シスター・クレア' },
-                    { key: 'hanabatake_chaika', name: '花畑チャイカ' },
-                    { key: 'yashiro_kizuku', name: '社築' },
-                    { key: 'azuchi_momo', name: '安土桃' },
-                    { key: 'suzuki_masaru', name: '鈴木勝' },
-                    { key: 'ryushen', name: '緑仙' },
-                    { key: 'uzuki_kou', name: '卯月コウ' },
-                    { key: 'hassaku_yuzu', name: '八朔ゆず' },
+                    { key: 'dola', name: 'ドーラ', icon: '/nijisanji/?plugin=ref&page=ドーラ&src=do-ra.png' },
+                    { key: 'umiyasyanokami', name: '海夜叉神', icon: '/nijisanji/?plugin=ref&page=海夜叉神&src=kami.png' },
+                    { key: 'nakao_azuma', name: '名伽尾アズマ', icon: '/nijisanji/?plugin=ref&page=名伽尾アズマ&src=azuma.png' },
+                    { key: 'izumo_kasumi', name: '出雲霞', icon: '/nijisanji/?plugin=ref&page=出雲霞&src=izumo.png' },
+                    { key: 'todoroki_kyouko', name: '轟京子', icon: '/nijisanji/?plugin=ref&page=轟京子&src=kyo.png' },
+                    { key: 'sister_cleaire', name: 'シスター・クレア', icon: '/nijisanji/?plugin=ref&page=シスター・クレア&src=kurea.png' },
+                    { key: 'hanabatake_chaika', name: '花畑チャイカ', icon: '/nijisanji/?plugin=ref&page=花畑チャイカ&src=chaika.png' },
+                    { key: 'yashiro_kizuku', name: '社築', icon: '/nijisanji/?plugin=ref&page=社築&src=syatiku.png' },
+                    { key: 'azuchi_momo', name: '安土桃', icon: '/nijisanji/?plugin=ref&page=安土桃&src=Adutimomo_400x400.jpg' },
+                    { key: 'suzuki_masaru', name: '鈴木勝', icon: '/nijisanji/?plugin=ref&page=漆黒の捕食者D.E.%28鈴木勝%29&src=ZJvfgjbr_400x400.jpg' },
+                    { key: 'ryushen', name: '緑仙', icon: '/nijisanji/?plugin=ref&page=緑仙&src=CQicycXP_400x400_0.jpg' },
+                    { key: 'uzuki_kou', name: '卯月コウ', icon: '/nijisanji/?plugin=ref&page=卯月コウ&src=_v3TMKaM_400x400_1.jpg' },
+                    { key: 'hassaku_yuzu', name: '八朔ゆず', icon: '/nijisanji/?plugin=ref&page=八朔ゆず&src=m3hTJp7B_400x400.jpg' },
                     // VOIZ
-                    { key: 'naruse_naru', name: '成瀬鳴' },
-                    { key: 'harusaki_air', name: '春崎エアル' },
+                    { key: 'naruse_naru', name: '成瀬鳴', icon: '/nijisanji/?plugin=ref&page=成瀬鳴&src=naruse.jpg' },
+                    { key: 'harusaki_air', name: '春崎エアル', icon: '/nijisanji/?plugin=ref&page=春崎エアル&src=harusaki.jpg' },
                     // COO
-                    { key: 'iwanaga', name: 'いわなが' },
+                    { key: 'iwanaga', name: 'いわなが', icon: '/nijisanji/?plugin=ref&page=いわながライブ&src=iwanaga.jpg' },
                 ];
                 this.settingKey = 'nijisanji_unofficial_wiki_extender_highlight_livers';
                 this.settings = new Set((localStorage.getItem(this.settingKey) || '').split(',').filter((key) => key));
             }
 
-            findKeyByNamePrefix(name) {
+            findByNamePrefix(name) {
                 return this.livers.find((liver) => name.startsWith(liver.name));
+            }
+
+            findByName(name) {
+                return this.livers.find((liver) => name === liver.name);
             }
 
             isHighlight(key) {
@@ -192,9 +213,62 @@
         if (now.getHours() >= 21) {
             const tomorrowDate = document.querySelector('#h2_content_1_1 + .date_weekday + .minicalendar_viewer + .date_weekday');
             const tomorrowSchedule = document.querySelector('#h2_content_1_1 + .date_weekday + .minicalendar_viewer + .date_weekday + .minicalendar_viewer ul');
+            tomorrowSchedule.classList.add('ex-tomorrow');
             targetTd.appendChild(tomorrowDate.cloneNode(true));
             targetTd.appendChild(tomorrowSchedule.cloneNode(true));
         }
+    }
+
+    function initializeNotificationTimer() {
+        while (timers.length > 0) {
+            const timer = timers.shift();
+            clearTimeout(timer);
+        }
+
+        const regexp = highlightSetting.getRegexp();
+
+        if (!regexp) {
+            return;
+        }
+
+        const now = new Date();
+        const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+        for (const li of Array.from(document.querySelectorAll('table[summary="calendar frame"] .ex-today > li'))) {
+            const innerText = normalizedElementText(li);
+            const match = innerText.match(regexp);
+            const date = parseDate(li, now);
+
+            if (match && date) {
+                date.setMinutes(date.getMinutes() - notifyBeforeMinutes);
+                if (date > now) {
+                    registerNotification(match[0], innerText, date - now);
+                }
+            }
+        }
+
+        for (const li of Array.from(document.querySelectorAll('table[summary="calendar frame"] .ex-tomorrow > li'))) {
+            const innerText = normalizedElementText(li);
+            const match = innerText.match(regexp);
+            const date = parseDate(li, tomorrow);
+            date.setMinutes(date.getMinutes() - notifyBeforeMinutes);
+
+            if (match && date) {
+                date.setMinutes(date.getMinutes() - notifyBeforeMinutes);
+                if (date > now) {
+                    registerNotification(match[0], innerText, date - now);
+                }
+            }
+        }
+    }
+
+    function registerNotification(name, body, miliseconds) {
+        const liver = highlightSetting.findByName(name);
+        const icon = liver ? liver.icon : '';
+
+        timers.push(setTimeout(() => {
+            new Notification(name, { icon, body });
+        }, miliseconds));
     }
 
     function highlightAndDarkenRecentSchedule() {
@@ -242,6 +316,7 @@
                 highlightSetting.toggleHighlight(key);
                 highlightAll();
                 highlightAndDarkenRecentSchedule();
+                initializeNotificationTimer();
             });
             return checkbox;
         }
@@ -253,7 +328,7 @@
             for (const td of Array.from(tr.querySelectorAll('td'))) {
                 const img = td.querySelector('img');
                 const alt = img && img.getAttribute('alt');
-                const liver = alt && highlightSetting.findKeyByNamePrefix(alt);
+                const liver = alt && highlightSetting.findByNamePrefix(alt);
 
                 td.innerText = '';
                 if (liver) {
@@ -327,12 +402,16 @@
         buttonWrapper.appendChild(button);
     }
 
-    function parseDate(li, base) {
-        const normalizedLi = li.cloneNode(true);
+    function normalizedElementText(ele) {
+        const normalizedElement = ele.cloneNode(true);
         // 打ち消し線の内容を消す
-        Array.from(normalizedLi.querySelectorAll('del')).forEach((del) => { del.innerText = '' });
+        Array.from(normalizedElement.querySelectorAll('del')).forEach((del) => { del.innerText = '' });
 
-        const match = normalizedLi.innerText.split('～')[0].match(/(\d{1,2})時(\d{1,2})分/);
+        return normalizedElement.innerText;
+    }
+
+    function parseDate(li, base) {
+        const match = normalizedElementText(li).split('～')[0].match(/(\d{1,2})時(\d{1,2})分/);
         if (!match) {
             return null;
         }
@@ -341,19 +420,31 @@
     }
 
     function reloadPage() {
-        return fetch(location.href).then((res) => res.text()).then((bodyText) => {
-            const parser = new DOMParser();
-            const body = parser.parseFromString(bodyText, 'text/html');
-            const target = document.querySelector("#body");
-
-            target.innerText = '';
-            for (const ele of Array.from(body.querySelector("#body").children)) {
-                target.appendChild(ele);
+        return Promise.new((resolve, reject) => {
+            // 通知タイミングちょうどにリロードしないようにずらす
+            const seconds = (new Date()).getSeconds();
+            if (seconds < 10) {
+                setTimeout(resolve, 10 - seconds);
+            } else if (seconds >= 10 && seconds < 50) {
+                resolve();
+            } else {
+                setTimeout(resolve, 70 - seconds);
             }
+        }).then(() => {
+            return fetch(location.href).then((res) => res.text()).then((bodyText) => {
+                const parser = new DOMParser();
+                const body = parser.parseFromString(bodyText, 'text/html');
+                const target = document.querySelector("#body");
 
-            const DOMContentLoaded_event = document.createEvent("Event");
-            DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
-            window.document.dispatchEvent(DOMContentLoaded_event);
+                target.innerText = '';
+                for (const ele of Array.from(body.querySelector("#body").children)) {
+                    target.appendChild(ele);
+                }
+
+                const DOMContentLoaded_event = document.createEvent("Event");
+                DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
+                window.document.dispatchEvent(DOMContentLoaded_event);
+            });
         });
     }
 })();
